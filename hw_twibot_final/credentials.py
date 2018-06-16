297933b7517097dbc13d6a320cac07223a1db90d
fishowl <fishow36@gmail.com>
2018-06-16 22:15:54 +0300

    real botgit add *!

---
+++ hw_twibot_final/credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'zvqgXIBRiUh0p9ujx3IgKqF7c'
+consumer_secret = 'aevPfoSRaML9VLZQBeIixpgdT7bo56j7L4tjW6T3jF9pcT2AJs'
+access_token = '1003542208841166849-UgV44RVcg23mFRntPb52HZUnljhMkq'
+access_token_secret = 'Nkd8C0dbQtd6Piqq79STEjBCamq3qatWjoZJzZXErSn0m'
